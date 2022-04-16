d52ab2c1b39aafdab6cabc49560b0ec9517cb6c9
Alex Todoran <todoran_alex@yahoo.com>
2022-04-16 20:57:10 +0300

    One line

--- models/index.js
+++ models/index.js
@@ -22,11 +22,10 @@ if (config.use_env_variable) {
 }
 
 fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
+  .filter(
+    (file) =>
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
+  )
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(
       sequelize,
